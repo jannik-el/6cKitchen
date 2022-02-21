1415221a10e03247ab3f0101f23550446e187a9b
Jannik <jels@itu.dk>
2022-02-21 19:06:24 +0100

    commit

---
+++ website.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+from streamlit_folium import folium_static
+import folium, json
+
+st.set_page_config(layout="wide")
+
+
+"# UMEUS 6C Kitchen's Website"
+
+
